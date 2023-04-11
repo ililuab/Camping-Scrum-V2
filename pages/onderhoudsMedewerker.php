4fcd3fd020868e3b1dea6c1cd1626eb32c738372
ililuab <1194420@student.roc-nijmegen.nl>
2023-04-11 09:48:19 +0200

    add bootstrap

--- pages/onderhoudsMedewerker.php
+++ pages/onderhoudsMedewerker.php
@@ -7,6 +7,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/styles.css">
 </head>
 <?php include "../inc/header.php";  ?>
